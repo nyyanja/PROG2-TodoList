80c2b08cab679badeac0256d467758a7b33864e1
nyyanja <nyanja.ramahavatra@gmail>
2025-08-20 11:44:37 +1100

    Feat : Add package entity ans class Todo

---
+++ src/main/java/com/nj/todolistapplication/entity/Todo.java
@@ -0,0 +1,37 @@
+package com.nj.todolistapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean completed = false;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
